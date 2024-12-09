cef31d219d18d05cc3d5890c46d0bf83283495b9
Anton <Antonhasagi@yandex.ru>
2024-12-09 22:01:32 +0300

    save_one

--- tests/main.py
+++ tests/main.py
@@ -1,7 +1,9 @@
 #вызываем функцию
 from src.masks import get_mask_card_number, get_mask_account
 
-
+a = "7000792289606361"
+b = "73654108430135874305"
 if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))
-    print(get_mask_account("73654108430135874305"))
+    print(get_mask_card_number(f"{a}"))
+    print(get_mask_account(f"{b}"))
+
