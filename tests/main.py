c3d47887cdff777a04cd4c6d90e26e523df3f412
Anton <Antonhasagi@yandex.ru>
2024-12-14 12:03:08 +0300

    init

--- tests/main.py
+++ tests/main.py
@@ -1,9 +1,8 @@
-#вызываем функцию
-from src.masks import get_mask_card_number, get_mask_account
+# вызываем функцию
+from src.widget import get_mask_account, get_mask_card_number
 
-a = "7000792289606361"
-b = "73654108430135874305"
+a = "Visa Platinum 7000792289606361"
+b = "Счёт 73654108430135874305"
 if __name__ == "__main__":
     print(get_mask_card_number(f"{a}"))
     print(get_mask_account(f"{b}"))
-
